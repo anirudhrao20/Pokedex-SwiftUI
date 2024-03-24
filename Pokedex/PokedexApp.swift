7667a9dbdac4f336fcadb28613d87b10a76ba60c
Anirudh Rao <contactme@anirudhrao.dev>
2024-03-24 13:52:21 -0700

    Initial Commit

---
+++ Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by Anirudh Rao on 3/24/24.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
